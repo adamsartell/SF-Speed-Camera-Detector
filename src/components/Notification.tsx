44d3b092a32f4cf79d413998b4cb67b6f2e599bb
adamsartell <adam.sartell@gmail.com>
2025-06-30 17:00:20 -0700

    adjust font weight for error notification

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -4,7 +4,7 @@ export default function Notification({
 	children: React.ReactNode;
 }) {
 	return (
-		<div className="absolute top-3 z-40 left-1/2 -translate-x-1/2 text-medium bg-[#f5f5f7]/60 border border-[#d6d6d6] backdrop-blur-xs p-4 rounded-lg shadow-lg flex gap-3 items-center w-[calc(100%-20px)] lg:w-fit">
+		<div className="absolute top-3 z-40 left-1/2 -translate-x-1/2 text-medium bg-[#f5f5f7]/60 border border-[#d6d6d6] backdrop-blur-xs p-4 rounded-lg shadow-lg flex gap-3 items-center w-[calc(100%-20px)] lg:w-fit font-medium">
 			{children}
 		</div>
 	);
